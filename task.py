9ce2b1090235df0e226e84a05d7c3ca192d09f0f
agustin <agustinchavero97@gmail.com>
2021-12-30 21:29:44 -0300

    Add constant variables. Rename functions

--- task.py
+++ task.py
@@ -7,70 +7,75 @@ from RPA.Archive import Archive
 from RPA.FileSystem import FileSystem
 import time
 
+MODAL_CONTAINER="css:div[class='container']"
+MODAL_HEADER="css:div[class='modal-header'"
+MODAL_OK_BUTTON="xpath://*[@class='btn btn-dark'][text()='OK']"
 
-def obtain_url():
+ROBOT_HEAD_LOCATOR="xpath://*[@class='custom-select'][@name='head']"
+ROBOT_LEG_LOCATOR="//*[@class='form-control'][@placeholder='Enter the part number for the legs']"
+ROBOT_ADDRESS_LOCATOR="//*[@class='form-control'][@placeholder='Shipping address']"
+ROBOT_PREVIEW_BUTTON="xpath://*[@id='preview'][@type='submit']"
+ROBOT_ORDER_BUTTON="xpath://*[@id='order'][text()='Order']"
+ROBOT_ORDER_ERROR="xpath://*[@class='alert alert-danger'][@role='alert']"
+ROBOT_SCREENSHOOT_PATH="output/images/robot{}-screenshoot.png"
+ROBOT_RECEIPT_PATH="./output/Order{}.pdf"
+
+def wait():
+    time.sleep(1)
+
+def obtainUrl():
     dialog = Dialogs()
     dialog.add_text("Porfavor ingrese url de archivo .csv")
     dialog.add_text_input("url")
     result = dialog.run_dialog()
     return(result["url"])
 
-def verify_modal():
-    browser.wait_until_element_is_enabled("css:div[class='container']")
-    if browser.is_element_enabled("css:div[class='modal-header'"):
-        browser.click_button("xpath://*[@class='btn btn-dark'][text()='OK']")
+def verifyModal():
+    browser.wait_until_element_is_enabled(MODAL_CONTAINER)
+    if browser.is_element_enabled(MODAL_HEADER):
+        browser.click_button(MODAL_OK_BUTTON)
 
-def download_csv():
-    url = obtain_url()
+def downloadCSV():
+    url = obtainUrl()
     http.download(url,target_file="output/SalesOrder.csv")
 
-def read_csv():
-    lcsv = Tables()
-    table = lcsv.read_table_from_csv("output/SalesOrder.csv")
+def readCSV():
+    tableLib = Tables()
+    table = tableLib.read_table_from_csv("output/SalesOrder.csv")
     table.group_by_column("Order number") #Necesary?
     return(table)
 
-def genero_robots():
-    table = read_csv()
-    browser.open_available_browser("https://robotsparebinindustries.com/#/robot-order",maximized=True)
-    for order in table:
-        verify_modal()
-        if browser.is_element_enabled("xpath://*[@class='custom-select'][@name='head']"):
-            browser.select_from_list_by_index("xpath://*[@class='custom-select'][@name='head']",order["Head"])
-            browser.select_radio_button("body",order["Body"])
-            browser.input_text("//*[@class='form-control'][@placeholder='Enter the part number for the legs']",order["Legs"])
-            browser.input_text("//*[@class='form-control'][@placeholder='Shipping address']",order["Address"])
-            browser.click_button("xpath://*[@id='preview'][@type='submit']")
-            time.sleep(1)
-            robotScreenshot = browser.screenshot("id:robot-preview-image",filename="output/images/robot{}-screenshoot.png".format(order["Order number"]))
-            browser.click_button("xpath://*[@id='order'][text()='Order']")
-            time.sleep(1)
-            visible_error = browser.is_element_visible("xpath://*[@class='alert alert-danger'][@role='alert']")
-            while visible_error:
-                browser.click_button("xpath://*[@id='order'][text()='Order']")
-                time.sleep(1)
-                if browser.is_element_visible("id:order-completion"):
-                    break
-            time.sleep(1)
-            receipt_table = browser.get_element_attribute("id:order-completion","outerHTML")
-            listOfFiles = []
-            listOfFiles.append("output/images/robot{}-screenshoot.png".format(order["Order number"]))
-            pdf.html_to_pdf(receipt_table,"./output/Order{}.pdf".format(order["Order number"]))
-            print(listOfFiles)
-            time.sleep(1)
-            pdf.open_pdf("./output/Order{}.pdf".format(order["Order number"]))
-            pdf.add_watermark_image_to_pdf(image_path="output/images/robot{}-screenshoot.png".format(order["Order number"]),output_path="./output/Order{}.pdf".format(order["Order number"]))
-            pdf.close_pdf("./output/Order{}.pdf".format(order["Order number"]))
-            time.sleep(1)
-            browser.click_button("id:order-another")
-            time.sleep(1)
-    browser.close_browser()
-    deleteFiles()
-    
+def fillForm(order):
+    if browser.is_element_enabled(ROBOT_HEAD_LOCATOR):
+        browser.select_from_list_by_index(ROBOT_HEAD_LOCATOR,order["Head"])
+        browser.select_radio_button("body",order["Body"])
+        browser.input_text(ROBOT_LEG_LOCATOR,order["Legs"])
+        browser.input_text(ROBOT_ADDRESS_LOCATOR,order["Address"])
+        browser.click_button(ROBOT_PREVIEW_BUTTON)
+        wait()
+        #Save Robot Preview Screenshoot
+        browser.screenshot("id:robot-preview-image",filename=ROBOT_SCREENSHOOT_PATH.format(order["Order number"]))
+        browser.click_button(ROBOT_ORDER_BUTTON)
+        wait()
+        visible_error = browser.is_element_visible(ROBOT_ORDER_ERROR)
+        while visible_error:
+            browser.click_button(ROBOT_ORDER_BUTTON)
+            wait()
+            if browser.is_element_visible("id:order-completion"):
+                break
+        wait()
 
-def deleteFiles():
-    archivo = Archive()
-    archivo.archive_folder_with_zip("output","./output/Orders.zip",include="*.pdf")
+def saveReceipt(order):
+    receipt_table = browser.get_element_attribute("id:order-completion","outerHTML")
+    pdf.html_to_pdf(receipt_table,ROBOT_RECEIPT_PATH.format(order["Order number"]))
+    wait()
+    pdf.open_pdf(ROBOT_RECEIPT_PATH.format(order["Order number"]))
+    pdf.add_watermark_image_to_pdf(image_path=ROBOT_SCREENSHOOT_PATH.format(order["Order number"]),output_path=ROBOT_RECEIPT_PATH.format(order["Order number"]))
+    pdf.close_pdf(ROBOT_RECEIPT_PATH.format(order["Order number"]))
+
+def cleanFiles():
+    archive = Archive()
+    archive.archive_folder_with_zip("output","./output/Orders.zip",include="*.pdf")
     fileSystem = FileSystem()
     pdfOrders = fileSystem.find_files("output/*.pdf")
     for pdf in pdfOrders:
@@ -78,13 +83,23 @@ def deleteFiles():
     pngRobots = fileSystem.find_files("output/images/*.png")
     for image in pngRobots:
         fileSystem.remove_file(image)
+
+def orderRobots():
+    orderTable = readCSV()
+    browser.open_available_browser("https://robotsparebinindustries.com/#/robot-order",maximized=True)
+    for order in orderTable:
+        verifyModal()
+        fillForm(order)
+        saveReceipt(order)
+        browser.click_button("id:order-another")
+    browser.close_browser()
+    
+
             
 if __name__ == "__main__":   
     browser = Selenium()
     http = HTTP()
     pdf = PDF()
-    #url = obtengo_url()
-    #http.download(url,overwrite=True)
-    genero_robots()
-    #descargo_csv()
-    #leo_csv()
+    downloadCSV()
+    orderRobots()
+    cleanFiles()
